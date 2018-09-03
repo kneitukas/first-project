a35a8edbee1568b856df09c4f236cd875b426085
kneitukas <nosheep6@gmail.comgit config --global user.email nosheep6@gmail.comgit config --global user.email nosheep6@gmail.com>
2018-09-03 11:49:51 +0300

    adding server.js

--- main/scripts.js
+++ main/scripts.js
@@ -1,12 +1,6 @@
-// function myMap() {
-//     var mapOptions = {
-//         center: new google.maps.LatLng(55.7241059,21.1268382),
-//         zoom: 18,
-//         mapTypeId: google.maps.MapTypeId.HYBRID,
-//         marker: new google.maps.LatLng(55.7241059,21.1268382)
-//     }   
-// var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-// }
+
+
+// Map
 
 function initMap() {
     var myLatLng = {lat: 55.724540, lng: 21.127071};
@@ -21,4 +15,38 @@ function initMap() {
       map: map,
       title: 'Hello World!'
     });
-  }
+  };
+
+
+// scroll effect
+// let $ = require('jquery');
+
+  $(document).ready(function() {
+  
+    var scrollLink = $('.scroll');
+    
+    // Smooth scrolling
+    scrollLink.click(function(e) {
+      e.preventDefault();
+      $('body,html').animate({
+        scrollTop: $(this.hash).offset().top
+      }, 1000 );
+    });
+    
+    // Active link switching
+    $(window).scroll(function() {
+      var scrollbarLocation = $(this).scrollTop();
+      
+      scrollLink.each(function() {
+        
+        var sectionOffset = $(this.hash).offset().top - 20;
+        
+        if ( sectionOffset <= scrollbarLocation ) {
+          $(this).parent().addClass('active');
+          $(this).parent().siblings().removeClass('active');
+        }
+      })
+      
+    })
+    
+  })
